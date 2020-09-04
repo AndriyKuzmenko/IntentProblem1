5e8638bf930615fa65a2cff0fbfeec43c14cb45b
Andriy <ak8022@bs.amalnet.k12.il>
2020-09-04 19:17:40 +0300

    Reading first number

--- app/src/main/java/com/example/intentproblem1/MainActivity.java
+++ app/src/main/java/com/example/intentproblem1/MainActivity.java
@@ -3,14 +3,45 @@ package com.example.intentproblem1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+    double a, b, answer;
+    char sign;
+    EditText display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display=(EditText)findViewById(R.id.display);
+    }
+
+    public void plus(View view)
+    {
+        a=Double.parseDouble(String.valueOf(display.getText()));
+        sign='+';
+    }
+
+    public void minus(View view)
+    {
+        a=Double.parseDouble(String.valueOf(display.getText()));
+        sign='-';
+    }
+
+    public void multiply(View view)
+    {
+        a=Double.parseDouble(String.valueOf(display.getText()));
+        sign='*';
+    }
+
+    public void divide(View view)
+    {
+        a=Double.parseDouble(String.valueOf(display.getText()));
+        sign='/';
     }
 }
